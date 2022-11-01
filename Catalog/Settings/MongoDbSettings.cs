2ec701da061059991568eec15f91b6ab36a59a73
Sarah Carla Souza <sarah.souza@foa.org.br>
2022-11-01 15:52:48 -0300

    added mongodb user and password

--- Catalog/Settings/MongoDbSettings.cs
+++ Catalog/Settings/MongoDbSettings.cs
@@ -5,11 +5,15 @@
         public string Host { get; set; }
         public int Port { get; set; }
 
+        // These two will be populated at runtime by .net
+        public string User { get; set; }
+        public string Password { get; set; }
+
         public string ConnectionString
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
