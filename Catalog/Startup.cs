1e83d5637c402640d0369e8314703e61af7d6159
Sarah Carla Souza <sarah.souza@foa.org.br>
2022-11-04 11:05:07 -0300

    added docker support

--- Catalog/Startup.cs
+++ Catalog/Startup.cs
@@ -77,8 +77,12 @@ namespace Catalog
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
